6d8f299584b797fe8af8ae1ef7f30e75cea1a67f
Alex Bravo <alexb760@gmail.com>
2024-09-11 12:58:17 -0500

    04_02: Adding equal simbol

--- learning-terraform-main/main.tf
+++ learning-terraform-main/main.tf
@@ -77,7 +77,7 @@ module "alb" {
     http = {
       port     = 80
       protocol = "HTTP"
-      default_action {
+      default_action = {
         type = "forward"
         target_group_arn = aws_lb_target_group.example.arn
       }

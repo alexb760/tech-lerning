124e87185580ec619b034fc854dca4164a65c094
Alex Bravo <alexb760@gmail.com>
2024-09-16 15:34:49 -0500

    04_02: replacing things from course.

--- learning-terraform-main/main.tf
+++ learning-terraform-main/main.tf
@@ -41,6 +41,40 @@ module "blog_vpc" {
   }
 }
 
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source   = "terraform-aws-modules/security-group/aws"
   version  = "4.13.0"
@@ -51,41 +85,4 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  # Security Group
-  security_groups  = module.blog_sg.security_group_id
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-      default_action = {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.example.arn
-      }
-    }
-  }
-
-  tags = {
-    Environment = "Development"
-    Project     = "Example"
-  }
 }
